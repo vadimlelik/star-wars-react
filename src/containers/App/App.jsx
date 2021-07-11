a46dcd90af2918ee952dbd7a107f935b9e8015c4
vadimcvirko <promiti@yandex.ru>
2021-07-12 00:43:39 +0300

    add className

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,11 @@
+import cn from 'classnames'
+import styles from './App.module.css'
+const App = () => {
+  return (
+    <div className={styles.header}>
+      Hello World
+    </div>
+  );
+}
+
+export default App;
