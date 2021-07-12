fa1d004e2c6a3f82c0349e51e2b3a64f0b2ce150
vadimcvirko <promiti90@gmail.com>
2021-07-12 11:56:33 +0300

    add  api

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,11 +1,8 @@
-import cn from 'classnames'
-import styles from './App.module.css'
+import styles from './App.module.css';
+import { getApiResourse } from '../../utils/network';
+
 const App = () => {
-  return (
-    <div className={styles.header}>
-      Hello World
-    </div>
-  );
-}
+  return <div className={styles.header}>Hello World</div>;
+};
 
 export default App;
---
+++ src/utils/network.js
@@ -0,0 +1,23 @@
+const SWAPI_ROOT = `https://swapi.dev/api/`;
+const SWAPI_PEPLE = `people`;
+
+export const getApiResourse = async (url) => {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      console.log('Could not fetch', res.status);
+      return false;
+    }
+    return await res.json();
+  } catch (error) {
+    console.error(error.message);
+    return false;
+  }
+};
+
+// getApiResourse(`${SWAPI_ROOT}${SWAPI_PEPLE}`).then((body) => console.log(body));
+
+(async ()=>{
+  const  body = await getApiResourse(`${SWAPI_ROOT}${SWAPI_PEPLE}`)
+  console.log(body);
+})();
