9fee25e4b754d505a51c7820f3e35ed6e0351e4d
vadim.cvirko <promiti90@gmail.com>
2021-07-20 12:48:13 +0300

    feat:refactory

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,15 +3,15 @@ import { NavLink } from 'react-router-dom';
 import styles from './Header.module.css';
 
 const Header = () => {
-    return (
-        <div className={styles.container}>
-            <ul className={styles.list__container}>
-                <li><NavLink to="/" exact>Home</NavLink></li>
-                <li><NavLink to="/people/?page=1" exact>People</NavLink></li>
-                <li><NavLink to="/not-found" exact>Not Found</NavLink></li>
-            </ul>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <ul className={styles.list__container}>
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/people/?page=1">People</NavLink></li>
+        <li><NavLink to="/not-found" exact>Not Found</NavLink></li>
+      </ul>
+    </div>
+  )
 }
 
 export default Header;
